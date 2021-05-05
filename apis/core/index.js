f3f0372a43509623832b2ace6d7d214ce27f7c29
Toby Miller <tobius@tobius.com>
2021-05-05 14:06:55 -0400

    Fix paren error

--- apis/core/index.js
+++ apis/core/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({ foo: 'World' });
+  res.write(JSON.stringify({ foo: 'World' }));
   res.end();
 }).listen(process.env.PORT || 5000);
